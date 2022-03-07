ce4a135617021c4e269b64e0da57308694b1cbe2
abigcatc683 <abigcatc683@SDS-L-763HC63>
2022-03-07 14:05:46 -0500

    Finished code

--- War/Form1.cs
+++ War/Form1.cs
@@ -14,12 +14,15 @@ namespace War
 {
     public partial class Form1 : Form
     {
-        //random generator for picking new cards   
+        //random generator for picking new cards
+        Random randGen = new Random();
 
         //variables to hold card values 
+        int playerCard, computerCard;
 
         //variables to hold scores, initialized to 0
-
+        int playerScore = 0;
+        int computerScore = 0;
 
         public Form1()
         {
@@ -33,24 +36,40 @@ namespace War
             cpuCardLabel.Image = null;
 
             //get random value between 1 and 10 for both player and cpu
+            playerCard = randGen.Next(1, 11);
+            computerCard = randGen.Next(1, 11);
 
-            
             //display card values to the labels
-
+            playerCardLabel.Text = $"{playerCard}";
+            cpuCardLabel.Text = $"{computerCard}";
 
             // if player value is greater then cpu card
             //    add to player score 
             //    show new player score 
             //    show message saying player won
-              
+
+            if (playerCard > computerCard) {
+                playerScore++;
+                playerScoreLabel.Text = $"{playerScore}";
+                outputLabel.Text = "Player wins";
+            }
+
             // if player value is less then cpu card
             //    add to cpu score
             //    show new cpu score
             //    show message saying cpu won
-              
+
+            else if (computerCard > playerCard) {
+                computerScore++;
+                cpuScoreLabel.Text = $"{computerScore}";
+                outputLabel.Text = "Computer wins :(";
+            }
+
             // if player value is equal to cpu value 
             //    show message that this round is a tie.                  
 
+            else {
+                outputLabel.Text = "It's a tie";
+            }
         }
-    }
 }
